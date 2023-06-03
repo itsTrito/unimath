aaa9e7ddf5cd32b77aa90ba079a0661a77847f2d
TristanBrault <trit0tv0@gmail.com>
2023-06-03 18:44:05 -0400

    c

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,5 @@
-msg = 'hello world'
-print(msg)
+from grapher import Grapher
+
+g = Grapher(['2 * x', '1 - ((x^2)/factorial(2)) + (x^4)/factorial(4)'],
+            lb=-3, ub=3, lol='lower center', title='Taylor Polynomial for cos(x)')
+g.plot()
